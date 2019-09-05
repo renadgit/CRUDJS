919bf0ce7eb611a8248cfa53560ad5506c16f616
Renad El Ashy <renad@Renads-MacBook-Air.local>
2019-09-05 15:57:35 +0400

    final version of API REST

---
+++ precache-manifest.113a1d828d190046d5336407588e714b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prj-one/robots.txt"
+  },
+  {
+    "revision": "f513e65bb5001ae5f68c",
+    "url": "/prj-one/js/chunk-vendors.0cddb425.js"
+  },
+  {
+    "revision": "50b40f2a64674d9f17ea",
+    "url": "/prj-one/js/app.d5880d81.js"
+  },
+  {
+    "revision": "26f3bf103e0fbe706fec",
+    "url": "/prj-one/js/about.35880a15.js"
+  },
+  {
+    "revision": "959d47628f8eb6e9e17ecc0466c6881f",
+    "url": "/prj-one/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/prj-one/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "50b40f2a64674d9f17ea",
+    "url": "/prj-one/css/app.9ffcd16b.css"
+  }
+];
